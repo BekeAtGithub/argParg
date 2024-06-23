834de7c52270b6e75eea4a23b01b6f0d6d4706b7
Adam Marton Beke <65732867+BekeAtGithub@users.noreply.github.com>
2024-06-23 11:24:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
 
     parser.add_argument('name', help="the name of the note")
     parser.add_argument('-f', '--folder', metavar="PATH", help='The folder the note is stored within')
-    parser.add_argument('-T', '--tag', metavar='TAG:TAG', help='colon seperated tags associated with the note')
+    parser.add_argument('-T', '--tag', metavar='TAG:TAG', help='Colon seperated tags associated with the note')
     parser.add_argument('--findTag', metavar="TAG:TAG", help='locates any notes with specific tags')
 
     args = parser.parse_args()
